97d708aa0e2b6ab3c6f3b4e46eec3c1afe925fde
jmptable <trueowen@verizon.net>
2013-02-03 05:27:36 -0500

    Test of thruster component using pygame to draw path of satellite.

--- test.py
+++ test.py
@@ -1,24 +1,41 @@
 import bfcpu
+from world import *
 import satellite 
 import components
 
-#p = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
-p = ".>.,"
-d = [1]
+import pygame
 
-b = bfcpu.BFCPU(512)
+screen = pygame.display.set_mode((1024, 600), pygame.FULLSCREEN)
+
+""" CPU STUFF """
+#p = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
+p = ".>.>."
+d = [2, 0, 32000]
+
+cpu = bfcpu.BFCPU(512)
 
 # load program
 for i, c in enumerate(p):
-	b.write(i, ord(c))
+	cpu.write(i, ord(c))
 
 # load data
 for i, v in enumerate(d):
-	b.write(i+128, v)
+	cpu.write(i+128, v)
 
-b.bus.add(components.Clock())
+""" CONSTRUCT SAT """
+world = World(128, 128, 128)
 
-sat = satellite.Satellite(b)
+
+sat = Satellite(cpu, (0, 0, 0), (0, 0, 0))
+cpu.bus.add(components.Thruster(sat))
+
+world.sats.append(sat)
 
 while(True):
-	sat.tick()
+	world.tick()
+	pygame.draw.circle(screen, (255, 255, 255), (sat.x+512, sat.y+300), 16, 3)
+	pygame.display.flip()
+
+	for event in pygame.event.get():
+		if(event.type==pygame.KEYDOWN):
+			exit()
