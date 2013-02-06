fd54f8d23bcd3c75cdd395ec4367ffb1783f6f2a
jmptable <trueowen@verizon.net>
2013-02-06 09:51:22 -0500

    Command base logic separated into its own module.

---
+++ command.py
@@ -0,0 +1,46 @@
+from abc import *
+import shlex
+
+man = {
+	'NAME':'',
+	'SYNOPSIS':'',
+	'DESCRIPTION':'',
+	'OPTIONS': {
+		'-h':'prints info about the command.'
+	},
+	'CAT':'',
+	'BUGS':''
+}
+
+# breaks string into individual options and arguments
+def decode(raw):
+	tokens = shlex.split(raw)
+
+	opts = {}
+	args = []
+	for t in tokens:
+		if(t.startswith('-')):
+			opt[t[1:]] = True
+		else:
+			args.append(t)
+
+	return {'options':opts, 'args':args}
+
+class Cmd(object):
+	def __init__(self, player, ansi):
+		self.player = player	# commands act on players
+		self.ansi = ansi		# input/output
+
+	# actuallly runs the command
+	@abstractmethod
+	def execute(self, raw_args):
+		arguments = decode(raw_args)
+		return
+
+	def error(self, msg):
+		self.ansi.color("red")
+		self.out("error: "+msg+"\n\r")
+		self.ansi.clear()
+
+	def out(self, msg):
+		self.ansi.out(msg)
