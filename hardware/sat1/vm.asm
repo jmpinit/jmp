abcdd259957aaf7e8b798c3263ec644cfdc6228a
jmptable <jmptable@mit.edu>
2013-03-10 12:15:00 -0400

    Enough VM to add two numbers.

--- hardware/sat1/vm.asm
+++ hardware/sat1/vm.asm
@@ -1,6 +1,46 @@
 .include "m32def.inc"
 
 ;**** Macros
+.macro inc_ptr
+	clr		r0
+	inc		@0L
+	adc		@0H, r0
+.endmacro
+
+.macro dec_ptr
+	clr		r0
+	dec		@0L
+	sbc		@0H, r0
+.endmacro
+
+.macro to_ptr
+	st		X+, YH
+	st		X, YL
+.endmacro
+
+.macro from_ptr
+	ld		YH, X+
+	ld		YL, X
+.endmacro
+
+.macro pc_to_X
+	ldi		XH, high(SRAM_START)
+	ldi		XL, low(SRAM_START)
+	add		XL, VM_PCL	; offset by VM's PC
+	adc		XH, VM_PCH
+.endmacro
+
+.macro ptr_to_X
+	mov		XL, @0L		; start w/ raw data ptr
+	mov		XH, @0H
+	lsl		XL			; multiply by 2 for byte offset
+	rol		XH
+	ldi		r16, low(SRAM_START)
+	add		XL, r16
+	ldi		r16, high(SRAM_START)
+	adc		XH, r16
+.endmacro
+
 .macro jumpto
 	ldi	ZL, low(@0)
 	ldi	ZH, high(@0)
@@ -11,16 +51,26 @@
 .endmacro
 
 ;**** Constants
+; VM's
 .include "vm_const.asm"
+.equ DATA_STACK		= 127
+.equ RET_STACK		= 255
+
+; general
 .equ BAUD	= 12 ; 38.4K baud
 
 ;**** Registers
-.def VM_PCL	= r15
-.def VM_PCH = r14
-.def VM_DPL	= r13
-.def VM_DPH	= r12
-.def VM_RPL	= r11
-.def VM_RPH	= r10
+; VM's
+.def VM_PCH	= r15
+.def VM_PCL = r14
+.def VM_DPH	= r13
+.def VM_DPL	= r12
+.def VM_RPH	= r11
+.def VM_RPL	= r10
+
+; general
+.def TEMPH	= r9
+.def TEMPL	= r8
 
 ;**** Pin definitions
 
@@ -83,6 +133,12 @@ jmptable:
 
 ; VM operations
 op_const:
+	inc_ptr		VM_PC
+	pc_to_X
+	from_ptr
+	rcall		vm_pushd
+	rjmp		tick_done3
+
 op_call:
 op_jump:
 op_jumpz:
@@ -90,15 +146,9 @@ op_jumpif:
 	rjmp	tick_done
 
 op_load:
-	ldi		r16, 'L'
-	rcall	ser_tx
-
 	rjmp	tick_done2
 
 op_stor:
-	ldi		r16, 'S'
-	rcall	ser_tx
-
 	rjmp	tick_done2
 
 op_return:
@@ -111,7 +161,19 @@ op_rts:
 	rjmp	tick_done
 
 op_add:
-	rjmp	tick_done
+	; retrieve params
+	rcall	vm_popd
+	movw	TEMPH:TEMPL, YH:YL
+	rcall	vm_popd
+
+	; add them
+	add		YL, TEMPL
+	adc		YH, TEMPH
+
+	; push result
+	rcall	vm_pushd
+
+	rjmp	tick_done5
 op_sub:
 op_mul:
 op_div:
@@ -126,14 +188,13 @@ op_sync:
 op_next:
 	rjmp	tick_done
 
+halt:
+	sbi		PORT_DEBUG, PIN_LED
+	rjmp	halt
+
 tick:
 	; get the next instruction
-	ldi		XH, high(SRAM_START)
-	ldi		XL, low(SRAM_START)
-
-	add		XL, VM_PCL	; offset by VM's PC
-	adc		XH, VM_PCH
-
+	pc_to_X
 	ld		r16, X
 
 	; check if done
@@ -142,6 +203,14 @@ tick:
 
 	; execute the instruction
 	jumpto	jmptable, r16
+tick_done5:		; increment PC by 5
+	clr		r0
+	inc		VM_PCL
+	adc		VM_PCH, r0
+tick_done4:		; increment PC by 3
+	clr		r0
+	inc		VM_PCL
+	adc		VM_PCH, r0
 tick_done3:		; increment PC by 2
 	clr		r0
 	inc		VM_PCL
@@ -151,32 +220,81 @@ tick_done2:		; increment PC by 1
 	inc		VM_PCL
 	adc		VM_PCH, r0
 tick_done:
-	ldi		r16, 10
-	rcall	delay
+	; ldi		r16, 10
+	; rcall	delay
+
+	ret
+
+;**** VM helper functions
+
+; pushes to VM data stack
+;INPUT: Y contains data to push
+vm_pushd:
+	; get RAM location of ptr
+	ptr_to_X	VM_DP
+	inc_ptr		VM_DP
+	rjmp	vm_push
+vm_pushr:
+	ptr_to_X	VM_RP
+	inc_ptr		VM_RP
+vm_push:
+	; store the val
+	to_ptr
+
+	ret
+
+; pops from VM data stack
+;OUTPUT: Y contains val
+vm_popd:
+	dec_ptr		VM_DP 
+	ptr_to_X	VM_DP
+	from_ptr
+
+	ret
+
+; pops from VM return stack
+;OUTPUT: r16 contains val
+vm_popr:
+	dec_ptr		VM_RP 
+	ptr_to_X	VM_RP
+
+	ld		YH, X+
+	ld		YL, X
 
 	ret
 
 ; loads program from PROGMEM into RAM
-load:
-	ldi		ZH, high(test_program<<1)
-	ldi		ZL, low(test_program<<1)
-
+;INPUT: Z is address in PROGMEM
+vm_load:
 	ldi		XH, high(SRAM_START)
 	ldi		XL, low(SRAM_START)
 
-	ldi		r17, 5
-load_loop:
+	ldi		r17, 8
+vm_load_loop:
 	lpm		r16, Z+
 	st		X+, r16
 	
 	dec		r17
-	brne	load_loop
+	brne	vm_load_loop
 
 	ret
 
-halt:
-	sbi		PORT_DEBUG, PIN_LED
-	rjmp	halt
+; resets the VM
+vm_init:
+	clr		VM_PCL
+	clr		VM_PCH
+
+	ldi		r16, low(DATA_STACK)
+	mov		VM_DPL, r16
+	ldi		r16, high(DATA_STACK)
+	mov		VM_DPH, r16
+
+	ldi		r16, low(RET_STACK)
+	mov		VM_RPL, r16
+	ldi		r16, high(RET_STACK)
+	mov		VM_RPH, r16
+
+	ret
 
 ;INPUT: r16 time
 ;DESTROYS: r0, r1, r2
@@ -220,12 +338,13 @@ reset:
 	ldi		r16, 'i'
 	rcall	ser_tx
 
+	; init the VM
+	rcall	vm_init
+
 	; load the test program
-	rcall	load
-
-	clr		VM_PCL
-	clr		VM_PCH
-
+	ldi		ZH, high(test_program<<1)
+	ldi		ZL, low(test_program<<1)
+	rcall	vm_load
 forever:
 	rcall	tick
 
@@ -235,5 +354,5 @@ forever:
 	rjmp	forever
 
 test_program:
-	.db		VM_OP_LOAD, VM_OP_STOR, VM_OP_LOAD, VM_OP_STOR, $FF, $FF
-	;.db		VM_OP_CONST, 56, VM_OP_CONST, 91, VM_OP_ADD, 255
+	;.db		VM_OP_LOAD, VM_OP_STOR, VM_OP_LOAD, VM_OP_STOR, $FF, $FF
+	.db		VM_OP_CONST, 0, 56, VM_OP_CONST, 0, 91, VM_OP_ADD, 255
